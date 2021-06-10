82cb0bb303d91687fed6371a965d76a696799d16
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-10 16:01:36 +0200

    result state and myAmount state added & formatted file

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,42 @@
+import { useState } from "react";
 import "./style.css";
 
 const Form = () => {
+    const [result, setResult] = useState("");
+    const [myAmount, setMyAmount] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        // setResult(calculateResult());
+    };
+
     return (
-        <form className="form js-form">
+        <form onSubmit={onFormSubmit} className="form js-form">
             <p>
                 <label>
-                    <strong className="form__inputDescription form__inputDescription--myAmount">Kwota:</strong>
-                    <input className="form__inputField form__inputField--myAmount js-form__myAmount" type="number" min="1" step="0.01"
-                        autoFocus required />
+                    <strong className="form__inputDescription form__inputDescription--myAmount"
+                    >
+                        Kwota:
+                    </strong>
+                    <input
+                        value={myAmount}
+                        onChange={({ target }) => setMyAmount(target.value)}
+                        className="form__inputField form__inputField--myAmount js-form__myAmount"
+                        type="number"
+                        min="1"
+                        step="0.01"
+                        autoFocus
+                        required
+                    />
                 </label>
             </p>
             <p>
                 <label>
-                    <span className="form__inputDescription">Przelicz z:</span>
+                    <span className="form__inputDescription">
+                        Przelicz z:
+                    </span>
                     <select className="form__inputField js-form__myCurrency">
                         <option value="EUR">EUR - Euro</option>
-                        <option value="NOK">NOK - Korona norweska</option>
-                        <option value="USD">USD - Amerykański dolar</option>
-                        <option value="PLN" selected>PLN - Polski złoty</option>
-                    </select>
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__inputDescription">Przelicz na:</span>
-                    <select className="form__inputField js-form__wantedCurrency">
-                        <option value="EUR" selected>EUR - Euro</option>
                         <option value="NOK">NOK - Korona norweska</option>
                         <option value="USD">USD - Amerykański dolar</option>
                         <option value="PLN">PLN - Polski złoty</option>
@@ -33,25 +44,61 @@ const Form = () => {
                 </label>
             </p>
             <p>
-                <label htmlFor="selectRate" className="form__screenReaderOnly">Ustawienia kursu</label>
-                <select id="selectRate" className="form__inputField form__inputField--select js-form__selectRate">
-                    <option value="defaultRate" selected>Kurs domyślny</option>
+                <label>
+                    <span className="form__inputDescription">Przelicz na:</span>
+                    <select className="form__inputField js-form__wantedCurrency">
+                        <option value="EUR">EUR - Euro</option>
+                        <option value="NOK">NOK - Korona norweska</option>
+                        <option value="USD">USD - Amerykański dolar</option>
+                        <option value="PLN">PLN - Polski złoty</option>
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label
+                    htmlFor="selectRate"
+                    className="form__screenReaderOnly"
+                >
+                    Ustawienia kursu
+                </label>
+                <select
+                    id="selectRate"
+                    className="form__inputField form__inputField--select js-form__selectRate"
+                >
+                    <option value="defaultRate">Kurs domyślny</option>
                     <option value="ownRate">Kurs własny</option>
                 </select>
-                <label htmlFor="exchangeRate" className="form__screenReaderOnly">Wysokość kursu</label>
-                <input id="exchangeRate" className="form__inputField form__inputField--notAllowed js-form__exchangeRate"
-                    type="number" min="0.01" step="0.0001" disabled required />
+                <label
+                    htmlFor="exchangeRate"
+                    className="form__screenReaderOnly"
+                >
+                    Wysokość kursu
+                </label>
+                <input
+                    id="exchangeRate"
+                    className="form__inputField form__inputField--notAllowed js-form__exchangeRate"
+                    type="number"
+                    min="0.01"
+                    step="0.0001"
+                    disabled
+                    required />
             </p>
             <p className="form__linkParagraph">
-                <a className="form__link" href="https://www.money.pl/pieniadze/nbp/srednie/" target="_blank"
-                    title="Kursy walut money.pl" rel="noreferrer noopener">Zobacz
+                <a
+                    className="form__link"
+                    href="https://www.money.pl/pieniadze/nbp/srednie/"
+                    target="_blank"
+                    title="Kursy walut money.pl"
+                    rel="noreferrer noopener"
+                >
+                    Zobacz
                     tabelę kursów NBP
                 </a> (otwiera się w nowej karcie)
             </p>
             <p>
                 <button className="form__button">Przelicz!</button>
             </p>
-            <p className="form__result js-form__finalResult"></p>
+            <p className="form__result js-form__finalResult">{result}</p>
         </form>
     )
 };
