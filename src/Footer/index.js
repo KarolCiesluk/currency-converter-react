c83c422eb0beefc724071c67e90cbef6f2549da6
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-10 15:25:37 +0200

    component Footer added

---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Footer = () => (
+    <footer className="footer">
+        <p className="footer__copyright">&copy; 2021 Karol Cieśluk</p>
+    </footer>
+);
+
+export default Footer;
