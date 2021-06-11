488a2fabf10bb95898d22b20809380f33f6ad84e
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-11 21:32:51 +0200

    created CurrencySelect component

---
+++ src/Form/CurrencySelect/index.js
@@ -0,0 +1,18 @@
+const CurrencySelect = ({ currency, description, onCurrencyChange, currencies }) => (
+    <p>
+        <label className="form__gridContainer">
+            <span className="form__inputDescription">{description}</span>
+            <select
+                value={currency}
+                onChange={onCurrencyChange}
+                className="form__inputField"
+            >
+                {currencies.map(({ name, fullName }) => (
+                    <option key={name} value={name}>{name} - {fullName}</option>
+                ))}
+            </select>
+        </label>
+    </p>
+);
+
+export default CurrencySelect;
