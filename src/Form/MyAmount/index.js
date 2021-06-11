e41b89ae18493ba1417d98e2a7fb234f6f7232ce
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-11 21:33:32 +0200

    created MyAmount component

---
+++ src/Form/MyAmount/index.js
@@ -0,0 +1,21 @@
+const MyAmount = ({ myAmount, setMyAmount }) => (
+    <p>
+        <label className="form__gridContainer">
+            <strong className="form__inputDescription form__inputDescription--myAmount">
+                Kwota:
+            </strong>
+            <input
+                value={myAmount}
+                onChange={({ target }) => setMyAmount(target.value)}
+                className="form__inputField form__inputField--myAmount"
+                type="number"
+                min="1"
+                step="0.01"
+                autoFocus
+                required
+            />
+        </label>
+    </p>
+);
+
+export default MyAmount;
