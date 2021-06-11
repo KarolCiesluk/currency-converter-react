ca5301e95df573cf5c697665007444315708c36f
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-11 21:33:20 +0200

    created Link component

---
+++ src/Form/Link/index.js
@@ -0,0 +1,17 @@
+const Link = () => (
+    <p className="form__linkParagraph">
+        <a
+            className="form__link"
+            href="https://www.money.pl/pieniadze/nbp/srednie/"
+            target="_blank"
+            title="Kursy walut money.pl"
+            rel="noreferrer noopener"
+        >
+            Zobacz
+            tabelę kursów NBP
+        </a> (otwiera się w nowej karcie)
+    </p>
+
+);
+
+export default Link;
