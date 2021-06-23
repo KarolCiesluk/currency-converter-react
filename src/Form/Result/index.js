9a6addd84f1cf3eded2651a2269898157e21c348
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-23 12:37:33 +0200

    reduced spaces

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,11 +1,11 @@
 const Result = ({ resultData }) => (
-    <p className="form__result">
-        {
-            resultData.finalAmount
-                ? `${resultData.myAmount}${resultData.myCurrency} = ${resultData.finalAmount}${resultData.wantedCurrency}`
-                : ""
-        }
-    </p>
+  <p className="form__result">
+    {
+      resultData.finalAmount
+        ? `${resultData.myAmount}${resultData.myCurrency} = ${resultData.finalAmount}${resultData.wantedCurrency}`
+        : ""
+    }
+  </p>
 );
 
 export default Result;
