a9392939dd6c8fef255073ca5e07817031a58205
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-28 20:28:10 +0200

    created an object currentDateData for currentDay, currentTime and currentDateTime

--- src/Form/Time/index.js
+++ src/Form/Time/index.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 
 const Time = () => {
   const [date, setDate] = useState(new Date());
-  const [currentDay, setCurrentDay] = useState();
-  const [currentTime, setCurrentTime] = useState();
-  const [currentDateTime, setCurrentDateTime] = useState();
+  // const [currentDay, setCurrentDay] = useState();
+  // const [currentTime, setCurrentTime] = useState();
+  // const [currentDateTime, setCurrentDateTime] = useState();
+  const [currentDateData, setCurrentDateData] = useState({});
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -15,27 +16,42 @@ const Time = () => {
   }, []);
 
   useEffect(() => {
-    setCurrentDay(date.toLocaleDateString(
+    // setCurrentDay(date.toLocaleDateString(
+    // undefined,
+    // {
+    //   weekday: "long",
+    //   day: "numeric",
+    //   month: "long",
+    //   }));
+
+    //   setCurrentTime(date.toLocaleTimeString());
+
+    //   setCurrentDateTime(date.toJSON());
+    const currentDateTime = date.toJSON();
+    const currentTime = date.toLocaleTimeString();
+    const currentDay = date.toLocaleDateString(
       undefined,
       {
         weekday: "long",
         day: "numeric",
         month: "long",
-      }));
+      });
 
-      setCurrentTime(date.toLocaleTimeString());
-
-      setCurrentDateTime(date.toJSON());
+    setCurrentDateData({
+      currentDateTime,
+      currentTime,
+      currentDay,
+    });
   }, [date]);
 
-    return (
-      <p className="form__time">
-        Dzisiaj jest{" "}
-        <time dateTime={currentDateTime}>
-          {currentDay}, {currentTime}
-        </time>
-      </p>
-    );
+  return (
+    <p className="form__time">
+      Dzisiaj jest{" "}
+      <time dateTime={currentDateData.currentDateTime}>
+        {currentDateData.currentDay}, {currentDateData.currentTime}
+      </time>
+    </p>
+  );
 };
 
 export default Time;
