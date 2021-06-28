91300c23f1608a54dcb6855e0637d7cffbe8e046
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-28 20:08:37 +0200

    made date to appear immediately

--- src/Form/Time/index.js
+++ src/Form/Time/index.js
@@ -1,45 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Time = () => {
-  const [date, setDate] = useState();
+  const [date, setDate] = useState(new Date());
+  const [currentDay, setCurrentDay] = useState();
+  const [currentTime, setCurrentTime] = useState();
+  const [currentDateTime, setCurrentDateTime] = useState();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const currentDate = new Date();
-
-      const currentDay = currentDate.toLocaleDateString(
-        undefined,
-        {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-        }
-      );
-      const currentTime = currentDate.toLocaleTimeString();
-      const currentDateTime = currentDate.toJSON();
-      
-      setDate({
-        currentDay,
-        currentTime,
-        currentDateTime,
-      });
+      setDate(new Date());
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  if (date) {
+  useEffect(() => {
+    setCurrentDay(date.toLocaleDateString(
+      undefined,
+      {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+      }));
+
+      setCurrentTime(date.toLocaleTimeString());
+
+      setCurrentDateTime(date.toJSON());
+  }, [date]);
+
     return (
       <p className="form__time">
         Dzisiaj jest{" "}
-        <time dateTime={date.currentDateTime}>
-          {date.currentDay}, {date.currentTime}
+        <time dateTime={currentDateTime}>
+          {currentDay}, {currentTime}
         </time>
       </p>
     );
-  }
-
-  return null;
 };
 
 export default Time;
