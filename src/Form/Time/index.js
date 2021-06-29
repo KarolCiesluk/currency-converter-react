396c9078391d415a5506aa9d965ac4bc442a0ab6
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-29 15:51:46 +0200

    deleted currentDateData and unneded useEffect

--- src/Form/Time/index.js
+++ src/Form/Time/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 const Time = () => {
   const [date, setDate] = useState(new Date());
-  const [currentDateData, setCurrentDateData] = useState({});
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -12,30 +11,21 @@ const Time = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  useEffect(() => {
-    const currentDay = date.toLocaleDateString(
-      undefined,
-      {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-      }
-    );
-    const currentTime = date.toLocaleTimeString();
-    const currentJSONDate = date.toJSON();
-
-    setCurrentDateData({
-      currentDay,
-      currentTime,
-      currentJSONDate,
-    });
-  }, [date]);
-
   return (
     <p className="form__time">
       Dzisiaj jest{" "}
-      <time dateTime={currentDateData.currentJSONDate}>
-        {currentDateData.currentDay}, {currentDateData.currentTime}
+      <time dateTime={date.toJSON()}>
+        {date.toLocaleDateString(
+          undefined,
+          {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          }
+        )}
       </time>
     </p>
   );
