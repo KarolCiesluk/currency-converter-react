b2a3886c7485147af30e908d5e5757b5fe824078
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-08 18:54:50 +0200

    added styled-components to Main

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,9 @@
-import "./style.css";
+import { Container } from "./styled";
 
 const Main = ({ children }) => (
-    <main className="main">
+    <Container>
         {children}
-    </main>
+    </Container>
 );
 
 export default Main;
---
+++ src/Main/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    max-width: 560px;
+    margin: 70px auto;
+    padding: 0 10px;
+`;
