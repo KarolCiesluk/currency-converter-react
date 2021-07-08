9a218fef88490aa039f1d3e6cd852e37a2f086e7
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-08 20:19:07 +0200

    deleted <p> in MyAmount and CurrencySelect

--- src/Form/CurrencySelect/index.js
+++ src/Form/CurrencySelect/index.js
@@ -1,20 +1,18 @@
 import { currencies } from "../currencies";
 
-const CurrencySelect = ({ description, currency, onCurrencyChange}) => (
-    <p>
-        <label className="form__gridContainer">
-            <span className="form__inputDescription">{description}</span>
-            <select
-                value={currency}
-                onChange={onCurrencyChange}
-                className="form__inputField"
-            >
-                {currencies.map(({ name, fullName }) => (
-                    <option key={name} value={name}>{name} - {fullName}</option>
-                ))}
-            </select>
-        </label>
-    </p>
+const CurrencySelect = ({ description, currency, onCurrencyChange }) => (
+    <label className="form__gridContainer">
+        <span className="form__inputDescription">{description}</span>
+        <select
+            value={currency}
+            onChange={onCurrencyChange}
+            className="form__inputField"
+        >
+            {currencies.map(({ name, fullName }) => (
+                <option key={name} value={name}>{name} - {fullName}</option>
+            ))}
+        </select>
+    </label>
 );
 
 export default CurrencySelect;
--- src/Form/MyAmount/index.js
+++ src/Form/MyAmount/index.js
@@ -1,21 +1,19 @@
 const MyAmount = ({ myAmount, setMyAmount }) => (
-    <p>
-        <label className="form__gridContainer">
-            <span className="form__inputDescription form__inputDescription--myAmount">
-                Kwota:
-            </span>
-            <input
-                value={myAmount}
-                onChange={({ target }) => setMyAmount(target.value)}
-                className="form__inputField form__inputField--myAmount"
-                type="number"
-                min="1"
-                step="0.01"
-                autoFocus
-                required
-            />
-        </label>
-    </p>
+    <label className="form__gridContainer">
+        <span className="form__inputDescription form__inputDescription--myAmount">
+            Kwota:
+        </span>
+        <input
+            value={myAmount}
+            onChange={({ target }) => setMyAmount(target.value)}
+            className="form__inputField form__inputField--myAmount"
+            type="number"
+            min="1"
+            step="0.01"
+            autoFocus
+            required
+        />
+    </label>
 );
 
 export default MyAmount;
