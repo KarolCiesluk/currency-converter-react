f127ec8e01a29d61dd0d96936630f023d9fbe138
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-09 12:00:11 +0200

    added theme to Footer

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.footer`
     margin: 0 40px;
     padding: 16px;
-    border-top: 1px solid hsl(3, 63%, 48%);
+    border-top: 1px solid ${({ theme }) => theme.persianRed.dark};
     text-align: center;
-    color: hsl(3, 63%, 48%);
+    color: ${({ theme }) => theme.persianRed.dark};
 `;
