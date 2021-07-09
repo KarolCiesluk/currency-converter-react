79ff30acaf5ce9e0f9f68e2da44a688b87ec6dca
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-09 08:48:23 +0200

    put all the colors in theme

--- src/App.js
+++ src/App.js
@@ -4,9 +4,44 @@ import Form from "./Form";
 import Footer from "./Footer";
 import { ThemeProvider } from "styled-components";
 
+const theme = {
+  headerColors: {
+    background: "#2f4858f7",
+    text: "#fcfcfc",
+  },
+  formColors: {
+    shadow: "#7a7a7a",
+    background: "#f2f3f4d9",
+    formControlsColors: {
+      inputBackground: "#fcfcfc",
+      inputText: "#2F4858",
+      inputFocus: "hsl(208deg 100% 30%)",
+      customRateBackground: "transparent",
+    },
+    linkColors: {
+      text: "hsl(208deg 100% 30%)",
+      active: "hsl(208deg 100% 45%)",
+    },
+    resultColors: {
+      text: "hsl(3, 63%, 48%)",
+    },
+    submitButtonColors: {
+      text: "#fcfcfc",
+      background: "hsl(3, 63%, 48%)",
+      hover: "hsl(3deg 100% 50%)",
+      focus: "hsl(3deg 100% 50%)",
+      active: "hsl(3, 63%, 58%)",
+    }
+  },
+  footerColors: {
+    border: "hsl(3, 63%, 48%)",
+    text: "hsl(3, 63%, 48%)",
+  },
+};
+
 function App() {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Header>
         Kalkulator walutowy
       </Header>
