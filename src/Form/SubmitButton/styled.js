c8167fcd26d44b95d8fa75b222f868179092e262
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-09 11:57:55 +0200

    added theme to SubmitButton

--- src/Form/SubmitButton/styled.js
+++ src/Form/SubmitButton/styled.js
@@ -2,23 +2,23 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     padding: 12px 50px;
-    background-color: hsl(3, 63%, 48%);
+    background-color: ${({ theme }) => theme.persianRed.dark};
     color: #fcfcfc;
     border: none;
-    box-shadow: 0px 0px 8px 0px #7a7a7a;
+    box-shadow: 0px 0px 8px 0px ${({ theme }) => theme.grey.dark};
     border-radius: 30px;
     outline: none;
     transition: background-color 0.3s;
 
     &:hover {
-        background-color: hsl(3deg 100% 50%);
+        background-color: ${({ theme }) => theme.persianRed.medium};
     }
 
     &:focus {
-        background-color: hsl(3deg 100% 50%);
+        background-color: ${({ theme }) => theme.persianRed.medium};
     }
 
     &:active {
-        background-color: hsl(3, 63%, 58%);
+        background-color: ${({ theme }) => theme.persianRed.light};
     }
 `;
