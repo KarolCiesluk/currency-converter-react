5568e60bfca4cff02d38a687ae0db5ea6d5df29a
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-09 08:24:02 +0200

    imported ThemeProvider to App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Header from "./Header";
 import Main from "./Main";
 import Form from "./Form";
 import Footer from "./Footer";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <Header>
         Kalkulator walutowy
       </Header>
@@ -13,7 +14,7 @@ function App() {
         <Form />
       </Main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
