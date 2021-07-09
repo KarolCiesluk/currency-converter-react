1b8635d9790a6220788dae4cc93bb732ba4ed3d2
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-09 12:08:33 +0200

    moved theme to separate file

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import Main from "./Main";
 import Form from "./Form";
 import Footer from "./Footer";
 import { ThemeProvider } from "styled-components";
-
-const theme = {
-  alabaster: "#fcfcfc",
-  pickledBluewood: {
-    dark: "#2f4858f7",
-    light: "#2F4858",
-  },
-  grey: {
-    dark: "#7a7a7a",
-    light: "#f2f3f4d9",
-  },
-  endeavour: {
-    dark: "#005299",
-    light: "#007ae6",
-  },
-  persianRed: {
-    dark: "#c8352d",
-    medium: "#ff0d00",
-    light: "#d75750",
-  },
-};
+import { theme } from "./theme";
 
 function App() {
   return (
