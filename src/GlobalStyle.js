e7723dc7aa2038a8a72d7102b12308ae3a370e55
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-14 13:38:43 +0200

    added GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+import backgroundImage from "./images/backgroundCash.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::before, ::after {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Open Sans', sans-serif;
+        font-size: 18px;
+        color: ${({theme}) => theme.pickledBluewood.light};
+        background-image: url(${backgroundImage});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        background-attachment: local; 
+        min-height: 100vh;
+    }
+`;
