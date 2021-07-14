3a8415fa3a90450fee303ea5569b007b78bb41e5
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-14 11:45:22 +0200

    updated ConverterContent

--- src/Form/ConverterContent/index.js
+++ src/Form/ConverterContent/index.js
@@ -4,7 +4,6 @@ import ExchangeRateChoice from "./ExchangeRateChoice";
 import ExchangeRatesInfo from "./ExchangeRatesInfo";
 import SubmitButton from "./SubmitButton";
 import Result from "./Result";
-import Message from "./Message";
 
 const ConverterContent = ({
   myAmount,
@@ -19,40 +18,34 @@ const ConverterContent = ({
   setCustomRate,
   resultData,
   exchangeRateAPI,
-  isError
 }) => (
   <>
-    {exchangeRateAPI ?
-      <>
-        <MyAmount
-          description="Kwota:"
-          myAmount={myAmount}
-          setMyAmount={setMyAmount}
-        />
-        <CurrencySelect
-          description="Przelicz z:"
-          currency={myCurrency}
-          onCurrencyChange={onMyCurrencyChange}
-          exchangeRatesAPI={exchangeRateAPI}
-        />
-        <CurrencySelect
-          description="Przelicz na:"
-          currency={wantedCurrency}
-          onCurrencyChange={onWantedCurrencyChange}
-          exchangeRatesAPI={exchangeRateAPI}
-        />
-        <ExchangeRateChoice
-          isCustomRate={isCustomRate}
-          onRateOptionChange={onRateOptionChange}
-          customRate={customRate}
-          setCustomRate={setCustomRate}
-        />
-        <ExchangeRatesInfo exchangeRateAPI={exchangeRateAPI} />
-        <SubmitButton buttonText="Przelicz!" />
-        <Result resultData={resultData} />
-      </> :
-      <Message isError={isError} />
-    }
+    <MyAmount
+      description="Kwota:"
+      myAmount={myAmount}
+      setMyAmount={setMyAmount}
+    />
+    <CurrencySelect
+      description="Przelicz z:"
+      currency={myCurrency}
+      onCurrencyChange={onMyCurrencyChange}
+      exchangeRatesAPI={exchangeRateAPI}
+    />
+    <CurrencySelect
+      description="Przelicz na:"
+      currency={wantedCurrency}
+      onCurrencyChange={onWantedCurrencyChange}
+      exchangeRatesAPI={exchangeRateAPI}
+    />
+    <ExchangeRateChoice
+      isCustomRate={isCustomRate}
+      onRateOptionChange={onRateOptionChange}
+      customRate={customRate}
+      setCustomRate={setCustomRate}
+    />
+    <ExchangeRatesInfo exchangeRateAPI={exchangeRateAPI} />
+    <SubmitButton buttonText="Przelicz!" />
+    <Result resultData={resultData} />
   </>
 );
 
